f7d299adbe1b150e08ae3952e59cbed6909e9a18
saeraniren <lentorek10130@gmail.com>
2024-05-15 22:58:43 +0900

    commit

--- src/components/search.js
+++ src/components/search.js
@@ -1,3 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './search.css';
+import React, { useState } from 'react';
+import './search.css'; // CSS 파일 import
 
+const SearchBar = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div className="search-bar-container">
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요..."
+        value={query}
+        onChange={handleInputChange}
+        className="search-input"
+      />
+      <button onClick={handleSearch} className="search-button">검색</button>
+    </div>
+  );
+};
+
+export default SearchBar;
