35a0ae31b1850215c3d8025af7c11bcc5b0b4657
saeraniren <lentorek10130@gmail.com>
2024-05-20 21:53:44 +0900

    update

--- src/App.js
+++ src/App.js
@@ -51,12 +51,49 @@ function App() {
 
   return (
     <div className="App">
-      <div className="nav">
-        <div className="logo">
-          프로젝트 로고
-        </div>
-        <Components.SearchBar />
+      <div className='nav-area'>
+        <div><h1>프로젝트 로고</h1></div>
+        <span>사업공고</span>
+        <span>커뮤니티</span>
+        <span>정책제도</span>
+        <span>지원바람</span>
       </div>
+      <hr/>
+      
+      <div className='search-area'>
+        <div>
+          <input
+            type="text"
+            placeholder='검색어를 입력하세요.'
+            className="search-input"
+            />
+          <button>검색</button>
+        </div>
+        <div className='filter-search'>
+          <span>지역별</span>
+          <span>분야별</span>
+          <span>모집기간별</span>
+        </div>
+      </div>
+      <hr/>
+
+      <div className='log-in-area'>
+        <form>
+          <input
+            type='text'
+            placeholder='ID'
+            className='ID'
+            />
+          <input
+            type='password'
+            placeholder='Password'
+            className='pwd'
+            />
+          <button>log in</button>
+        </form>
+      </div>
+      <hr/>
+
     </div>
   );
 }
